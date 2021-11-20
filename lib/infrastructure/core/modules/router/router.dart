917420127149541f465b5cc2d1fa437672efeac0
Talha <talha.kerpicci@gmail.com>
2021-11-20 18:49:29 +0300

    Added signup screen

--- lib/infrastructure/core/modules/router/router.dart
+++ lib/infrastructure/core/modules/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:house_of_auctions/presentation/screens/authentication/sign_up_screen.dart';
 import 'package:house_of_auctions/presentation/screens/intro_screen.dart';
 import 'package:house_of_auctions/presentation/screens/splash_screen.dart';
 
@@ -7,6 +8,7 @@ import 'package:house_of_auctions/presentation/screens/splash_screen.dart';
   routes: [
     MaterialRoute(page: SplashScreen, initial: true),
     MaterialRoute(page: IntroScreen),
+    MaterialRoute(page: SignUpScreen),
   ],
 )
 class $AppRouter {}
