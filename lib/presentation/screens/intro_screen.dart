56b5e249f04ffaec24551c668ce5ea83de61474b
Talha <talha.kerpicci@gmail.com>
2021-11-20 18:49:43 +0300

    Configuring slides

--- lib/presentation/screens/intro_screen.dart
+++ lib/presentation/screens/intro_screen.dart
@@ -1,4 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:house_of_auctions/domain/models/core/app_data_model.dart';
+import 'package:house_of_auctions/infrastructure/core/di/di.dart';
+import 'package:house_of_auctions/infrastructure/core/helpers/app_helper_functions.dart';
+import 'package:house_of_auctions/infrastructure/core/modules/data_storage/data_storage.dart';
+import 'package:house_of_auctions/infrastructure/core/modules/router/router.gr.dart';
 import 'package:intro_slider/intro_slider.dart';
 import 'package:intro_slider/slide_object.dart';
 
@@ -10,69 +16,22 @@ class IntroScreen extends StatefulWidget {
 }
 
 class IntroScreenState extends State<IntroScreen> {
+  final _appRouter = AppRouter();
   List<Slide> slides = [];
 
   @override
   void initState() {
     super.initState();
-
-    slides.add(
-      Slide(
-        widgetTitle: const Text(
-          'Welcome',
-          maxLines: 2,
-          /* style: const TextStyle(
-            color: AppColors.nord1,
-            fontSize: 22,
-          ), */
-          textAlign: TextAlign.center,
-        ),
-        description: 'Description',
-        /* pathImage: '', */
-        /* backgroundColor: Colors.white,
-        styleTitle: const TextStyle(
-          color: AppColors.nord1,
-          fontSize: 22,
-        ),
-        styleDescription: const TextStyle(
-          color: AppColors.nord1,
-          fontSize: 14,
-          height: 1.4,
-        ), */
-      ),
-    );
-    slides.add(
-      Slide(
-        widgetTitle: const Text(
-          'Easily Bid',
-          maxLines: 2,
-          /* style: const TextStyle(
-            color: AppColors.nord1,
-            fontSize: 22,
-          ), */
-          textAlign: TextAlign.center,
-        ),
-        description: 'Win ez',
-        /* pathImage: '', */
-        backgroundColor: Colors.white,
-        /* styleTitle: const TextStyle(
-          color: AppColors.nord1,
-          fontSize: 22,
-        ),
-        styleDescription: const TextStyle(
-          color: AppColors.nord1,
-          fontSize: 14,
-          height: 1.4,
-        ), */
-      ),
-    );
   }
 
   Future<void> onDonePress() async {
-    /* var prefs = await SharedPreferences.getInstance();
-    prefs.setBool('showSliders', false);
+    await getIt<HiveDataStorage>().write(
+      const AppDataModel(showIntro: false),
+    );
 
-    Get.offAll(() => WelcomeScreen()); */
+    await _appRouter.replace(
+      const SignUpScreenRoute(),
+    );
   }
 
   Widget renderDoneBtn() {
@@ -111,6 +70,45 @@ class IntroScreenState extends State<IntroScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = getTextTheme(context);
+    slides.add(
+      Slide(
+        widgetTitle: Text(
+          'Welcome to House of Auctions',
+          maxLines: 2,
+          style: theme.headline2,
+          textAlign: TextAlign.center,
+        ),
+        description: 'House of Auctions is a an app where rich ppl get to get everything',
+        pathImage: 'assets/images/sliders/business.png',
+        backgroundColor: Colors.white,
+        /* styleTitle: theme.headline2!.copyWith(color: AppColors.yellow),
+        styleDescription: const TextStyle(
+          color: AppColors.yellow,
+          fontSize: 14,
+          height: 1.4,
+        ), */
+      ),
+    );
+    slides.add(
+      Slide(
+        widgetTitle: Text(
+          'Just Bid More!',
+          maxLines: 2,
+          style: theme.headline2,
+          textAlign: TextAlign.center,
+        ),
+        description: 'Bid more to lose more!',
+        pathImage: 'assets/images/sliders/payment.png',
+        backgroundColor: Colors.white,
+        /* styleTitle: theme.headline2!.copyWith(color: AppColors.yellow),
+        styleDescription: const TextStyle(
+          color: AppColors.yellow,
+          fontSize: 14,
+          height: 1.4,
+        ), */
+      ),
+    );
     return IntroSlider(
       slides: slides,
       onDonePress: onDonePress,
