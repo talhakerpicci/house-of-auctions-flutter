9ec6f068fa7053d7fe111afb10204b8956fa7905
Talha <talha.kerpicci@gmail.com>
2021-11-27 19:42:51 +0300

    Looks better

--- lib/presentation/screens/intro_screen.dart
+++ lib/presentation/screens/intro_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:house_of_auctions/domain/models/core/app_data_model.dart';
+import 'package:house_of_auctions/infrastructure/core/constants/colors.dart';
 import 'package:house_of_auctions/infrastructure/core/di/di.dart';
 import 'package:house_of_auctions/infrastructure/core/helpers/app_helper_functions.dart';
 import 'package:house_of_auctions/infrastructure/core/modules/data_storage/data_storage.dart';
@@ -16,7 +17,6 @@ class IntroScreen extends StatefulWidget {
 }
 
 class IntroScreenState extends State<IntroScreen> {
-  final _appRouter = AppRouter();
   List<Slide> slides = [];
 
   @override
@@ -29,41 +29,33 @@ class IntroScreenState extends State<IntroScreen> {
       const AppDataModel(showIntro: false),
     );
 
-    await _appRouter.replace(
-      const SignUpScreenRoute(),
-    );
+    await context.router.replace(const SignUpScreenRoute());
   }
 
   Widget renderDoneBtn() {
-    return const Text(
+    return Text(
       'Get Started',
-      /* style: const TextStyle(
-        color: AppColors.nord1,
-      ), */
       textAlign: TextAlign.center,
+      style: getTextTheme(context).subtitle1!.copyWith(color: AppColors.blue),
     );
   }
 
   Widget renderSkipBtn() {
-    return const FittedBox(
+    return FittedBox(
       fit: BoxFit.fitWidth,
       child: Text(
         'Skip',
-        /* style: const TextStyle(
-          color: AppColors.nord1,
-        ), */
+        style: getTextTheme(context).subtitle1!.copyWith(color: AppColors.blue),
       ),
     );
   }
 
   Widget renderNextBtn() {
-    return const FittedBox(
+    return FittedBox(
       fit: BoxFit.fitWidth,
       child: Text(
         'Next',
-        /* style: const TextStyle(
-          color: AppColors.nord1,
-        ), */
+        style: getTextTheme(context).subtitle1!.copyWith(color: AppColors.blue),
       ),
     );
   }
@@ -76,18 +68,13 @@ class IntroScreenState extends State<IntroScreen> {
         widgetTitle: Text(
           'Welcome to House of Auctions',
           maxLines: 2,
-          style: theme.headline2,
           textAlign: TextAlign.center,
+          style: theme.headline1!.copyWith(color: AppColors.blue),
         ),
         description: 'House of Auctions is a an app where rich ppl get to get everything',
         pathImage: 'assets/images/sliders/business.png',
         backgroundColor: Colors.white,
-        /* styleTitle: theme.headline2!.copyWith(color: AppColors.yellow),
-        styleDescription: const TextStyle(
-          color: AppColors.yellow,
-          fontSize: 14,
-          height: 1.4,
-        ), */
+        styleDescription: theme.bodyText1!.copyWith(color: AppColors.darkGrey),
       ),
     );
     slides.add(
@@ -95,18 +82,13 @@ class IntroScreenState extends State<IntroScreen> {
         widgetTitle: Text(
           'Just Bid More!',
           maxLines: 2,
-          style: theme.headline2,
           textAlign: TextAlign.center,
+          style: theme.headline1!.copyWith(color: AppColors.blue),
         ),
         description: 'Bid more to lose more!',
         pathImage: 'assets/images/sliders/payment.png',
         backgroundColor: Colors.white,
-        /* styleTitle: theme.headline2!.copyWith(color: AppColors.yellow),
-        styleDescription: const TextStyle(
-          color: AppColors.yellow,
-          fontSize: 14,
-          height: 1.4,
-        ), */
+        styleDescription: theme.bodyText1!.copyWith(color: AppColors.darkGrey),
       ),
     );
     return IntroSlider(
