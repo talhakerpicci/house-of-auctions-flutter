d2ba1a8dc1664a6ecf9d077d933df3c3b30054dc
Talha <talha.kerpicci@gmail.com>
2021-11-27 19:42:38 +0300

    Added enw routes

--- lib/infrastructure/core/modules/router/router.dart
+++ lib/infrastructure/core/modules/router/router.dart
@@ -1,7 +1,11 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:house_of_auctions/app_navigator.dart';
+import 'package:house_of_auctions/presentation/screens/authentication/login_screen.dart';
 import 'package:house_of_auctions/presentation/screens/authentication/sign_up_screen.dart';
+import 'package:house_of_auctions/presentation/screens/authentication/welcome_screen.dart';
 import 'package:house_of_auctions/presentation/screens/intro_screen.dart';
+import 'package:house_of_auctions/presentation/screens/items/add_item_screen.dart';
 import 'package:house_of_auctions/presentation/screens/splash_screen.dart';
 
 @MaterialAutoRouter(
@@ -9,6 +13,10 @@ import 'package:house_of_auctions/presentation/screens/splash_screen.dart';
     MaterialRoute(page: SplashScreen, initial: true),
     MaterialRoute(page: IntroScreen),
     MaterialRoute(page: SignUpScreen),
+    MaterialRoute(page: WelcomeScreen),
+    MaterialRoute(page: LoginScreen),
+    MaterialRoute(page: AppNavigator),
+    MaterialRoute(page: AddItemScreen),
   ],
 )
 class $AppRouter {}
