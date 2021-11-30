ba71821a1d8d736168a486b0e065262bb87612c3
Talha <talha.kerpicci@gmail.com>
2021-11-30 23:31:03 +0300

    Added listener

--- lib/app_handler.dart
+++ lib/app_handler.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:house_of_auctions/application/authentication/auth_provider.dart';
+import 'package:house_of_auctions/application/base/base_provider.dart';
 import 'package:house_of_auctions/infrastructure/core/di/di.dart';
 import 'package:house_of_auctions/infrastructure/core/helpers/log/log_it.dart';
 import 'package:house_of_auctions/infrastructure/core/modules/data_storage/data_storage.dart';
 import 'package:house_of_auctions/infrastructure/core/modules/router/router.gr.dart';
+import 'package:provider/provider.dart';
 
 class AppHandler extends StatefulWidget {
   const AppHandler({
@@ -62,6 +65,7 @@ class _AppHandlerState extends State<AppHandler> {
     if (result.showIntro) {
       isInitialRun = false;
     }
+
     Future.delayed(const Duration(milliseconds: 2000), () {
       if (isInitialRun) {
         AutoRouter.of(widget.navigatorKey.currentContext!).replace(
@@ -71,21 +75,19 @@ class _AppHandlerState extends State<AppHandler> {
       }
     });
 
-    getIt<LogIt>().error(isInitialRun);
-
-    return widget.screen; /* BlocListener<AuthCubit, AuthState>(
-      listener: (context, state) {
-        if (state is Authenticated) {
+    return Consumer<AuthProvider>(
+      builder: (_, value, __) {
+        if (value.state == ViewState.ready) {
           AutoRouter.of(widget.navigatorKey.currentContext!).replace(
             const AppNavigatorRoute(),
           );
-        } else if (state is Unauthenticated) {
+        } else if (value.state == ViewState.error) {
           AutoRouter.of(widget.navigatorKey.currentContext!).replace(
-            const LoginScreenRoute(),
+            const WelcomeScreenRoute(),
           );
         }
+        return widget.screen;
       },
-      child: widget.screen,
-    ); */
+    );
   }
 }
