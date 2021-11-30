5c9a789adb82b3ec7a0901c2a8b623c52e330619
Talha <talha.kerpicci@gmail.com>
2021-11-30 23:31:25 +0300

    Bug fixes

--- lib/infrastructure/core/modules/api_client/api_client.dart
+++ lib/infrastructure/core/modules/api_client/api_client.dart
@@ -1,30 +1,22 @@
 import 'package:dio/dio.dart';
 import 'package:fresh_dio/fresh_dio.dart';
-import 'package:house_of_auctions/domain/models/core/token_model.dart';
 import 'package:house_of_auctions/infrastructure/core/constants/di.dart';
+import 'package:house_of_auctions/infrastructure/core/modules/token_storage/token_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 @lazySingleton
 class ApiClient {
   final Dio _dio = Dio();
-  final TokenStorage<TokenModel> storage;
+  final HiveTokenStorage storage;
 
   ApiClient({required this.storage}) {
+    final tokenModel = storage.read();
+
     _dio.options.baseUrl = env.apiBaseUrl;
     _dio.options.connectTimeout = const Duration(seconds: 10).inMilliseconds;
     _dio.options.receiveTimeout = const Duration(seconds: 5).inMilliseconds;
-    _dio.interceptors.add(
-      Fresh<TokenModel>(
-        tokenStorage: storage,
-        refreshToken: _refreshAccessToken,
-        tokenHeader: (token) {
-          return {
-            'authorization': 'Bearer ${token.accessToken}',
-          };
-        },
-      ),
-    );
+    if (tokenModel != null) _dio.options.headers['authorization'] = 'Bearer ${tokenModel.accessToken}';
 
     if (env.debugApiClient) {
       _dio.interceptors.add(PrettyDioLogger(
@@ -33,23 +25,6 @@ class ApiClient {
         responseHeader: true,
       ));
     }
-  }
-
-  Future<TokenModel> _refreshAccessToken(TokenModel? token, Dio dio) async {
-    // TODO: Implement refresh token
-    if (token != null) {
-      /* final result = await getIt<OpenIdConnectService>().refreshAccessToken(token.refreshToken); */
-      var result;
-
-      if (result != null) {
-        return TokenModel(
-          accessToken: result.accessToken!,
-          refreshToken: result.refreshToken!,
-        );
-      }
-    }
-
-    throw RevokeTokenException();
   }
 
   Future<Response> post(String path, dynamic data) async {
