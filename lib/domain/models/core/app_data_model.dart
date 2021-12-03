ba1140a81e2dde9995eee9440bc502b26ae35204
Talha <talha.kerpicci@gmail.com>
2021-12-03 22:39:25 +0300

    Changed parameter name

--- lib/domain/models/core/app_data_model.dart
+++ lib/domain/models/core/app_data_model.dart
@@ -6,10 +6,10 @@ part 'app_data_model.g.dart';
 @freezed
 class AppDataModel with _$AppDataModel {
   const factory AppDataModel({
-    required bool showIntro,
+    required bool skipIntro,
   }) = _AppDataModel;
 
-  factory AppDataModel.initial() => const AppDataModel(showIntro: true);
+  factory AppDataModel.initial() => const AppDataModel(skipIntro: false);
 
   factory AppDataModel.fromJson(Map<String, dynamic> json) => _$AppDataModelFromJson(json);
 }
