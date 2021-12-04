0f0ad81bbeaeb50700cc58cf8dbbe9b53c9a9e92
Talha <talha.kerpicci@gmail.com>
2021-12-04 20:48:58 +0300

    Added logout implementation

--- lib/presentation/screens/user/user_profile_screen.dart
+++ lib/presentation/screens/user/user_profile_screen.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:house_of_auctions/application/authentication/auth_provider.dart';
 
-class UserProfileScreen extends StatefulWidget {
+class UserProfileScreen extends ConsumerStatefulWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
 
   @override
   _UserProfileScreenState createState() => _UserProfileScreenState();
 }
 
-class _UserProfileScreenState extends State<UserProfileScreen> {
+class _UserProfileScreenState extends ConsumerState<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: TextButton(
-          onPressed: () {},
+          onPressed: () async {
+            await ref.read(authStateNotifierProvider.notifier).logout();
+          },
           child: const Text('Logout'),
         ),
       ),
