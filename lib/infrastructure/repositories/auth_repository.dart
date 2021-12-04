4c3f3562b7d27739b4112dd82e53e95d51f88a1b
Talha <talha.kerpicci@gmail.com>
2021-12-04 20:48:08 +0300

    Added dio error checker

--- lib/infrastructure/repositories/auth_repository.dart
+++ lib/infrastructure/repositories/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:data_channel/data_channel.dart';
+import 'package:dio/dio.dart';
 import 'package:house_of_auctions/domain/interfaces/i_auth_repository.dart';
 import 'package:house_of_auctions/domain/models/core/alert_model.dart';
 import 'package:house_of_auctions/domain/models/core/token_model.dart';
@@ -28,7 +29,15 @@ class AuthRepository implements IAuthRepository {
 
       return DC.data(tokenModel);
     } catch (e) {
-      final _alert = AlertModel(message: e.toString(), type: AlertType.error);
+      String errorMessage;
+
+      if (e is DioError) {
+        errorMessage = (e.response!.data as Map)['message'];
+      } else {
+        errorMessage = e.toString();
+      }
+
+      final _alert = AlertModel(message: errorMessage, type: AlertType.error);
 
       return DC.error(_alert);
     }
@@ -39,6 +48,7 @@ class AuthRepository implements IAuthRepository {
     try {
       final data = model.toJson();
       data['password'] = password;
+
       final result = await _apiClient.post('/register', data);
       final tokenModel = TokenModel(
         accessToken: (result.data as Map)['access_token'],
@@ -47,7 +57,15 @@ class AuthRepository implements IAuthRepository {
 
       return DC.data(tokenModel);
     } catch (e) {
-      final _alert = AlertModel(message: e.toString(), type: AlertType.error);
+      String errorMessage;
+
+      if (e is DioError) {
+        errorMessage = (e.response!.data as Map)['message'];
+      } else {
+        errorMessage = e.toString();
+      }
+
+      final _alert = AlertModel(message: errorMessage, type: AlertType.error);
 
       return DC.error(_alert);
     }
