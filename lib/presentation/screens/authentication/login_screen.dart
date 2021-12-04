fdfcce3ab3304d7efd8a86645c6f8cadafa3768d
Talha <talha.kerpicci@gmail.com>
2021-12-04 20:48:42 +0300

    Finished implementation

--- lib/presentation/screens/authentication/login_screen.dart
+++ lib/presentation/screens/authentication/login_screen.dart
@@ -1,29 +1,46 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:house_of_auctions/application/authentication/auth_provider.dart';
 import 'package:house_of_auctions/infrastructure/core/constants/colors.dart';
 import 'package:house_of_auctions/infrastructure/core/helpers/app_helper_functions.dart';
-import 'package:house_of_auctions/infrastructure/core/modules/router/router.gr.dart';
 import 'package:house_of_auctions/presentation/widgets/core/button.dart';
 import 'package:house_of_auctions/presentation/widgets/core/text_field.dart';
 import 'package:house_of_auctions/presentation/widgets/spaces.dart';
-import 'package:loading_overlay/loading_overlay.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  String email = '';
+  String password = '';
+
+  final _form = GlobalKey<FormState>();
+
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    emailController.value = TextEditingValue(text: email, selection: emailController.selection);
+    passwordController.value = TextEditingValue(text: password, selection: passwordController.selection);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: LoadingOverlay(
-        isLoading: false,
-        child: GestureDetector(
-          onTap: unFocus,
+      body: GestureDetector(
+        onTap: unFocus,
+        child: Form(
+          key: _form,
           child: ListView(
             children: <Widget>[
               const SpaceH40(),
@@ -46,7 +63,20 @@ class _LoginScreenState extends State<LoginScreen> {
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 hintText: 'Email',
                 prefixIcon: const Icon(Icons.email),
-                onChanged: (String value) {},
+                focusNode: emailFocusNode,
+                controller: emailController,
+                onChanged: (String value) {
+                  email = value;
+                },
+                onFieldSubmitted: (_) {
+                  requestFocus(context, passwordFocusNode);
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email cant be empty';
+                  }
+                  return null;
+                },
               ),
               const SpaceH10(),
               CustomTextField(
@@ -54,16 +84,37 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 50,
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 hintText: 'Password',
+                focusNode: passwordFocusNode,
+                controller: passwordController,
                 prefixIcon: const Icon(Icons.vpn_key),
-                onChanged: (String value) {},
+                onChanged: (String value) {
+                  password = value;
+                },
+                onFieldSubmitted: (_) {
+                  unFocus();
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Password cant be empty';
+                  }
+                  return null;
+                },
               ),
               const SpaceH16(),
               CustomButton(
                 color: AppColors.blue,
                 fullWidth: false,
                 buttonPadding: const EdgeInsets.symmetric(horizontal: 20),
-                onPressed: () {
-                  context.router.replaceAll([const AppNavigatorRoute()]);
+                onPressed: () async {
+                  /* context.router.replaceAll([const AppNavigatorRoute()]); */
+                  final isValid = _form.currentState!.validate();
+                  if (!isValid) {
+                    return;
+                  }
+                  await ref.read(authStateNotifierProvider.notifier).login(
+                        email: email,
+                        password: password,
+                      );
                 },
                 child: Center(
                   child: Text(
