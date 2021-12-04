05e5f079979b6244dca6416c86065c1b91ebf14f
Talha <talha.kerpicci@gmail.com>
2021-12-04 20:47:02 +0300

    Added AlertModel

--- lib/application/authentication/auth_state.dart
+++ lib/application/authentication/auth_state.dart
@@ -2,7 +2,7 @@ part of 'auth_provider.dart';
 
 @freezed
 class AuthState with _$AuthState {
-  const factory AuthState.authenticated() = Authenticated;
+  const factory AuthState.authenticated({AlertModel? alert}) = Authenticated;
   const factory AuthState.unauthenticated() = Unauthenticated;
   const factory AuthState.loading() = AuthLoading;
   const factory AuthState.failed({required AlertModel alert}) = AuthFailed;
