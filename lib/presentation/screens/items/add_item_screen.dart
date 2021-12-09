ae2843a6e5b80155fddf61a0bd06534dd8261d45
Talha <talha.kerpicci@gmail.com>
2021-12-10 00:07:57 +0300

    Progressing

--- lib/presentation/screens/items/add_item_screen.dart
+++ lib/presentation/screens/items/add_item_screen.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_auctions/infrastructure/core/helpers/app_helper_functions.dart';
+import 'package:house_of_auctions/presentation/widgets/core/sliver_app_bar.dart';
 
 class AddItemScreen extends StatelessWidget {
   const AddItemScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: SliverAppBarWidget(
+                children: [
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                  Container(
+                    height: 60,
+                    width: 40,
+                    color: Colors.red,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('button')),
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                  ListTile(
+                    title: Text('asdas'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              width: double.maxFinite,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Post'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
