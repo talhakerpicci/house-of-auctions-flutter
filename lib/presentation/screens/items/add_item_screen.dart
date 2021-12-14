3d68cb0ff5cd96c0ee04a708ca68acbec63fadbe
Talha <talha.kerpicci@gmail.com>
2021-12-14 14:41:08 +0300

    Progress

--- lib/presentation/screens/items/add_item_screen.dart
+++ lib/presentation/screens/items/add_item_screen.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_auctions/infrastructure/core/constants/colors.dart';
 import 'package:house_of_auctions/infrastructure/core/helpers/app_helper_functions.dart';
+import 'package:house_of_auctions/presentation/widgets/core/button.dart';
 import 'package:house_of_auctions/presentation/widgets/core/sliver_app_bar.dart';
+import 'package:house_of_auctions/presentation/widgets/spaces.dart';
 
 class AddItemScreen extends StatelessWidget {
   const AddItemScreen({Key? key}) : super(key: key);
+
+  Widget plusButton() {
+    return InkWell(
+      borderRadius: BorderRadius.circular(10),
+      onTap: () async {},
+      child: Container(
+        height: 80,
+        width: 80,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: AppColors.blue),
+        ),
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,42 +35,43 @@ class AddItemScreen extends StatelessWidget {
             Expanded(
               child: SliverAppBarWidget(
                 children: [
-                  ListTile(
-                    title: Text('asdas'),
-                  ),
-                  Container(
-                    height: 60,
-                    width: 40,
-                    color: Colors.red,
-                  ),
-                  ElevatedButton(onPressed: () {}, child: Text('button')),
-                  ListTile(
-                    title: Text('asdas'),
-                  ),
-                  ListTile(
-                    title: Text('asdas'),
-                  ),
-                  ListTile(
-                    title: Text('asdas'),
-                  ),
-                  ListTile(
-                    title: Text('asdas'),
-                  ),
-                  ListTile(
-                    title: Text('asdas'),
-                  ),
-                  ListTile(
-                    title: Text('asdas'),
+                  const SpaceH20(),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        const SpaceW20(),
+                        plusButton(),
+                        const SpaceW10(),
+                        plusButton(),
+                        const SpaceW10(),
+                        plusButton(),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
             SizedBox(
-              height: 50,
+              height: 80,
               width: double.maxFinite,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Post'),
+              child: Column(
+                children: [
+                  const Divider(),
+                  CustomButton(
+                    buttonPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                    color: AppColors.blue,
+                    onPressed: () {},
+                    child: Center(
+                      child: Text(
+                        'Post',
+                        style: getTextTheme(context).headline6!.copyWith(
+                              color: Colors.white,
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
