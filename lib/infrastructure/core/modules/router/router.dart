f1eb2acc068546d2a77ac0f557b127bc2e107b3b
Talha <talha.kerpicci@gmail.com>
2021-12-17 18:48:38 +0300

    Addec chat detail screen

--- lib/infrastructure/core/modules/router/router.dart
+++ lib/infrastructure/core/modules/router/router.dart
@@ -5,6 +5,7 @@ import 'package:house_of_auctions/app_navigator.dart';
 import 'package:house_of_auctions/presentation/screens/authentication/login_screen.dart';
 import 'package:house_of_auctions/presentation/screens/authentication/sign_up_screen.dart';
 import 'package:house_of_auctions/presentation/screens/authentication/welcome_screen.dart';
+import 'package:house_of_auctions/presentation/screens/chat/chat_detail_screen.dart';
 import 'package:house_of_auctions/presentation/screens/intro_screen.dart';
 import 'package:house_of_auctions/presentation/screens/items/add_item_screen.dart';
 import 'package:house_of_auctions/presentation/screens/splash_screen.dart';
@@ -16,7 +17,7 @@ import 'package:house_of_auctions/presentation/screens/user/user_profile_screen.
       page: AppHandler,
       initial: true,
       children: [
-        MaterialRoute(page: SplashScreen, initial: true),
+        MaterialRoute(page: SplashScreen),
         MaterialRoute(page: IntroScreen),
         MaterialRoute(page: SignUpScreen),
         MaterialRoute(page: WelcomeScreen),
@@ -24,6 +25,7 @@ import 'package:house_of_auctions/presentation/screens/user/user_profile_screen.
         MaterialRoute(page: AppNavigator),
         MaterialRoute(page: AddItemScreen),
         MaterialRoute(page: UserProfileScreen),
+        MaterialRoute(page: ChatDetailScreen),
       ],
     ),
   ],
