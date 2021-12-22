ed0ed054998df16dc09b6987be55a0b59d203839
Talha Kerpicci <talha.kerpicci@gmail.com>
2021-12-22 19:25:19 +0300

    Converted to listview builder

--- lib/presentation/screens/user/items_selling_screen.dart
+++ lib/presentation/screens/user/items_selling_screen.dart
@@ -6,14 +6,35 @@ class ItemsSellingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final itemList = <Map>[
+      {
+        'name': 'Vantuz S6 Elektrikli Süpürge',
+        'picture': 'https://6rk3rbju.rocketcdn.com/image/cdndata/185/1211103/image153.01.06.4925-3.jpg',
+        'price': '2200 Tl',
+        'numOfBids': '6',
+      },
+      {
+        'name': 'Sari Mont',
+        'picture': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDQYQur6M8dRM3DUew1ZMn2kP7sqUWjBtoeg&usqp=CAU',
+        'price': '365 Tl',
+        'numOfBids': '23',
+      },
+      {
+        'name': 'Sony SP400 Kulaklik',
+        'picture': 'https://techcrunch.com/wp-content/uploads/2020/11/DSC09882.jpg?w=1024',
+        'price': '850 Tl',
+        'numOfBids': '11',
+      },
+    ];
     return Scaffold(
-      body: ListView(
+      body: ListView.builder(
         padding: const EdgeInsets.only(
           top: 10,
         ),
-        children: const [
-          ItemSellingCard(),
-        ],
+        itemCount: itemList.length,
+        itemBuilder: (context, index) => ItemSellingCard(
+          itemDetail: itemList[index],
+        ),
       ),
     );
   }
