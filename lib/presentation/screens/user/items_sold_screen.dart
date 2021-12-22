46ba50d999e5776d0e689e798fc7eb518e5262ce
Talha Kerpicci <talha.kerpicci@gmail.com>
2021-12-22 19:25:24 +0300

    Converted to listview builder

--- lib/presentation/screens/user/items_sold_screen.dart
+++ lib/presentation/screens/user/items_sold_screen.dart
@@ -6,14 +6,29 @@ class ItemsSoldScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final itemList = <Map>[
+      {
+        'name': 'Dell Gaming G15 5510',
+        'picture': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjefFODtEBb6DmzzXDGw1nktatDmcRnaUbFRzOfWb4ouztBjVSxa9YYA0XKpbezu8oTLw&usqp=CAU',
+        'price': '13200 Tl',
+        'soldDate': 'Sold at 12/10/21',
+      },
+      {
+        'name': 'RX 580 Ekran Karti',
+        'picture': 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/msi/thumb/v2-86368-9_large.jpg',
+        'price': '9200 Tl',
+        'soldDate': 'Sold at 29/12/21',
+      },
+    ];
     return Scaffold(
-      body: ListView(
+      body: ListView.builder(
         padding: const EdgeInsets.only(
           top: 10,
         ),
-        children: const [
-          ItemSoldCard(),
-        ],
+        itemCount: itemList.length,
+        itemBuilder: (context, index) => ItemSoldCard(
+          itemDetail: itemList[index],
+        ),
       ),
     );
   }
