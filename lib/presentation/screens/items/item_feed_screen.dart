49311e09768f0ce3b1cf565a460748a2d49183d5
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-11 16:34:28 +0300

    Implemented items api

--- lib/presentation/screens/items/item_feed_screen.dart
+++ lib/presentation/screens/items/item_feed_screen.dart
@@ -1,56 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:house_of_auctions/application/items/items_provider.dart';
 import 'package:house_of_auctions/presentation/widgets/core/search_app_bar_widget.dart';
 import 'package:house_of_auctions/presentation/widgets/item/item_card.dart';
 
-class ItemFeedScreen extends StatefulWidget {
+class ItemFeedScreen extends ConsumerWidget {
   const ItemFeedScreen({Key? key}) : super(key: key);
 
   @override
-  _ItemFeedScreenState createState() => _ItemFeedScreenState();
-}
-
-class _ItemFeedScreenState extends State<ItemFeedScreen> {
-  List<String> urls = [
-    'https://pngimg.com/uploads/iphone_13/iphone_13_PNG5.png',
-    'https://pngimg.com/uploads/laptop/laptop_PNG101764.png',
-    'https://pngimg.com/uploads/headphones/headphones_PNG101980.png',
-    'https://pngimg.com/uploads/photo_camera/photo_camera_PNG101639.png',
-    'https://pngimg.com/uploads/bmw/bmw_PNG99566.png',
-    'https://pngimg.com/uploads/backpack/backpack_PNG6343.png',
-    'https://pngimg.com/uploads/hoodie/hoodie_PNG45.png',
-  ];
-  List<String> prices = [
-    '9500 TL',
-    '13240 TL',
-    '560 TL',
-    '1200 TL',
-    '350000 TL',
-    '96 TL',
-    '125 TL',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(itemsStateNotifierProvider);
     return Scaffold(
       appBar: const SearchAppbarWidget(),
-      body: RefreshIndicator(
-        onRefresh: () => Future.value(null),
-        child: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 18,
-            mainAxisSpacing: 18,
-            childAspectRatio: 0.75,
+      body: state.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        loaded: (items) => RefreshIndicator(
+          onRefresh: () => ref.read(itemsStateNotifierProvider.notifier).getItems(),
+          child: GridView.builder(
+            padding: const EdgeInsets.all(10),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 18,
+              mainAxisSpacing: 18,
+              childAspectRatio: 0.75,
+            ),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return ItemCard(
+                url: '',
+                price: items[index].initialPrice.toString(),
+              );
+            },
           ),
-          itemCount: urls.length,
-          itemBuilder: (context, index) {
-            return ItemCard(
-              url: urls[index],
-              price: prices[index],
-            );
-          },
         ),
+        failed: (alert) => const Center(child: Text('An Error Occured')),
       ),
     );
   }
