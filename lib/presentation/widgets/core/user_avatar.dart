b35bfd6d53c9fb7d5fd7d97be535dd397ac4afb9
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-11 22:25:29 +0300

    Refactoring

--- lib/presentation/widgets/core/user_avatar.dart
+++ lib/presentation/widgets/core/user_avatar.dart
@@ -1,10 +1,8 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:house_of_auctions/application/user/user_provider.dart';
 import 'package:house_of_auctions/infrastructure/core/constants/app_theme.dart';
 import 'package:house_of_auctions/infrastructure/core/constants/colors.dart';
-import 'package:house_of_auctions/infrastructure/core/helpers/bar/bar_helper.dart';
 
 enum AvatarSize { small, medium, large, xLarge }
 
@@ -16,56 +14,63 @@ class UserAvatar extends ConsumerWidget {
     this.size = AvatarSize.large,
   }) : super(key: key);
 
-  Widget buildAvatar(UserState state, BuildContext context) {
-    if (state is UserLoading) {
-      return const CircularProgressIndicator();
-    } else if (state is UserLoaded) {
-      final nameSurname = state.user.nameSurname.split(' ');
-      final firstName = nameSurname[0][0].toUpperCase();
-      final lastName = nameSurname.length > 1 ? nameSurname[1][0].toUpperCase() : '';
-      return Text(
-        '$firstName$lastName',
-        style: AppTheme.defaultTextStyle.copyWith(
-          fontSize: getFontSize(),
-          color: AppColors.lightGrey.withOpacity(0.8),
+  Widget buildAvatar({required UserState state, required BuildContext context, required Function()? onPressed}) {
+    return state.when(
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      loaded: (user) {
+        final nameSurname = user.nameSurname.split(' ');
+        final firstName = nameSurname[0][0].toUpperCase();
+        final lastName = nameSurname.length > 1 ? nameSurname[1][0].toUpperCase() : '';
+        return Text(
+          '$firstName$lastName',
+          style: AppTheme.defaultTextStyle.copyWith(
+            fontSize: getFontSize(),
+            color: AppColors.lightGrey.withOpacity(0.8),
+          ),
+        );
+      },
+      failed: (alert) => Center(
+        child: InkWell(
+          onTap: onPressed,
+          child: const Icon(
+            Icons.warning,
+            color: Colors.white,
+          ),
         ),
-      );
-    } else {
-      BarHelper.showAlert(
-        AutoRouter.of(context).navigatorKey.currentContext!,
-        alert: (state as UserFailed).alert,
-        showAboveBottomBar: true,
-      );
-      return const SizedBox();
-    }
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final backgroundColor = AppColors.getRandomColor();
-    return Consumer(builder: (context, ref, _) {
-      final state = ref.watch(userStateNotifierProvider);
-      return Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: AppColors.lightGrey,
+    final state = ref.watch(userStateNotifierProvider);
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColors.lightGrey,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.darkGrey.withOpacity(0.1),
+            blurRadius: 2,
+            offset: const Offset(0, 1),
           ),
-          boxShadow: [
-            BoxShadow(
-              color: AppColors.darkGrey.withOpacity(0.1),
-              blurRadius: 2,
-              offset: const Offset(0, 1),
-            ),
-          ],
-          borderRadius: BorderRadius.circular(24),
+        ],
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: CircleAvatar(
+        backgroundColor: backgroundColor,
+        radius: getSize(),
+        child: buildAvatar(
+          context: context,
+          state: state,
+          onPressed: () => ref.read(userStateNotifierProvider.notifier).getUserDetails(),
         ),
-        child: CircleAvatar(
-          backgroundColor: backgroundColor,
-          radius: getSize(),
-          child: buildAvatar(state, context),
-        ),
-      );
-    });
+      ),
+    );
   }
 
   double getSize() {
