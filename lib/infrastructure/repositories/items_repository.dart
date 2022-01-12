c6384e16c61d30913f007c28e6131d89f662fb73
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-12 17:31:06 +0300

    Implemented upload picture api

--- lib/infrastructure/repositories/items_repository.dart
+++ lib/infrastructure/repositories/items_repository.dart
@@ -4,6 +4,7 @@ import 'package:house_of_auctions/domain/interfaces/i_items_repository.dart';
 import 'package:house_of_auctions/domain/models/core/alert_model.dart';
 import 'package:house_of_auctions/domain/models/item/item_model.dart';
 import 'package:house_of_auctions/infrastructure/core/modules/api_client/api_client.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: IItemsRepository)
@@ -20,6 +21,34 @@ class ItemsRepository implements IItemsRepository {
       final items = (list['items'] as List).map((json) => ItemModel.fromJson(json)).toList();
 
       return DC.data(items);
+    } catch (e) {
+      String errorMessage;
+
+      if (e is DioError && e.response != null) {
+        errorMessage = (e.response!.data as Map)['message'];
+      } else {
+        errorMessage = e.toString();
+      }
+
+      final _alert = AlertModel(message: errorMessage, type: AlertType.error);
+
+      return DC.error(_alert);
+    }
+  }
+
+  @override
+  Future<DC<AlertModel, void>> uplaodPicture(XFile file, String fileName) async {
+    try {
+      final formData = FormData.fromMap({
+        'file': await MultipartFile.fromFile(file.path, filename: file.name),
+      });
+
+      await _apiClient.post(
+        '/upload-image',
+        formData,
+      );
+
+      return DC.data(null);
     } catch (e) {
       String errorMessage;
 
