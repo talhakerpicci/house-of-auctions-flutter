ecb05f11d10b5f0d7d3c29f48602ff93538d17ae
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-12 17:30:43 +0300

    Added upload picture

--- lib/domain/interfaces/i_items_repository.dart
+++ lib/domain/interfaces/i_items_repository.dart
@@ -1,7 +1,9 @@
 import 'package:data_channel/data_channel.dart';
 import 'package:house_of_auctions/domain/models/core/alert_model.dart';
 import 'package:house_of_auctions/domain/models/item/item_model.dart';
+import 'package:image_picker/image_picker.dart';
 
 abstract class IItemsRepository {
   Future<DC<AlertModel, List<ItemModel>>> getItems();
+  Future<DC<AlertModel, void>> uplaodPicture(XFile file, String fileName);
 }
