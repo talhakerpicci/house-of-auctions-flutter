dcbd74488d6c5ee1d29c40240c655351502aa96d
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-14 17:07:29 +0300

    Small fixes

--- lib/domain/interfaces/i_user_repository.dart
+++ lib/domain/interfaces/i_user_repository.dart
@@ -1,10 +1,11 @@
 import 'package:data_channel/data_channel.dart';
 import 'package:house_of_auctions/domain/models/core/alert_model.dart';
-import 'package:house_of_auctions/domain/models/core/token_model.dart';
 import 'package:house_of_auctions/domain/models/user/user_model.dart';
+import 'package:image_picker/image_picker.dart';
 
 abstract class IUserRepository {
   Future<DC<AlertModel, UserModel>> getUserDetails();
-  Future<DC<AlertModel, TokenModel>> updateUserInfo({required UserModel model});
-  Future<DC<AlertModel, TokenModel>> updatePassword({required UserModel model});
+  Future<DC<AlertModel, UserModel>> updateUserInfo({required UserModel model});
+  Future<DC<AlertModel, bool>> updateUserPicture({required XFile file});
+  Future<DC<AlertModel, bool>> updatePassword({required String newPassword});
 }
