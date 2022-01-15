7f40991171284379e8e51b263042c7aaac9bb03e
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-15 20:10:19 +0300

    Added listener

--- lib/presentation/screens/items/item_feed_screen.dart
+++ lib/presentation/screens/items/item_feed_screen.dart
@@ -1,6 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:house_of_auctions/application/add_item/add_item_provider.dart';
 import 'package:house_of_auctions/application/items/items_provider.dart';
+import 'package:house_of_auctions/infrastructure/core/helpers/bar/bar_helper.dart';
 import 'package:house_of_auctions/presentation/screens/error_screen.dart';
 import 'package:house_of_auctions/presentation/widgets/core/search_app_bar_widget.dart';
 import 'package:house_of_auctions/presentation/widgets/item/item_card.dart';
@@ -12,6 +15,17 @@ class ItemFeedScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(itemsStateNotifierProvider);
     /* ref.read(itemsStateNotifierProvider.notifier).getItems(); */
+    ref.listen(addItemStateNotifierProvider, (stateBefore, stateAfter) {
+      if (stateAfter is ItemSuccess) {
+        Navigator.pop(context);
+        ref.read(itemsStateNotifierProvider.notifier).getItems();
+        BarHelper.showAlert(
+          AutoRouter.of(context).navigatorKey.currentContext!,
+          alert: stateAfter.alert,
+          showAboveBottomBar: true,
+        );
+      }
+    });
     return Scaffold(
       appBar: const SearchAppbarWidget(),
       body: state.when(
