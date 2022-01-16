ac4f369d3dd8b1efd17b708ab2b006f2ec06f961
Talha Kerpicci <talha.kerpicci@gmail.com>
2022-01-16 16:05:37 +0300

    Implemented api

--- lib/presentation/screens/user/items_selling_screen.dart
+++ lib/presentation/screens/user/items_selling_screen.dart
@@ -1,39 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:house_of_auctions/application/items/items_provider.dart';
+import 'package:house_of_auctions/application/user/user_provider.dart';
+import 'package:house_of_auctions/domain/models/user/user_model.dart';
+import 'package:house_of_auctions/presentation/screens/empty_screen.dart';
+import 'package:house_of_auctions/presentation/screens/error_screen.dart';
 import 'package:house_of_auctions/presentation/widgets/item/item_selling_card.dart';
 
-class ItemsSellingScreen extends StatelessWidget {
+class ItemsSellingScreen extends ConsumerWidget {
   const ItemsSellingScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final itemList = <Map>[
-      {
-        'name': 'Vantuz S6 Elektrikli Süpürge',
-        'picture': 'https://6rk3rbju.rocketcdn.com/image/cdndata/185/1211103/image153.01.06.4925-3.jpg',
-        'price': '2200 Tl',
-        'numOfBids': '6',
-      },
-      {
-        'name': 'Sari Mont',
-        'picture': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDQYQur6M8dRM3DUew1ZMn2kP7sqUWjBtoeg&usqp=CAU',
-        'price': '365 Tl',
-        'numOfBids': '23',
-      },
-      {
-        'name': 'Sony SP400 Kulaklik',
-        'picture': 'https://techcrunch.com/wp-content/uploads/2020/11/DSC09882.jpg?w=1024',
-        'price': '850 Tl',
-        'numOfBids': '11',
-      },
-    ];
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userState = ref.watch(userStateNotifierProvider);
+    final user = userState is UserLoaded ? userState.user : UserModel.initial();
+    final state = ref.watch(itemsStateNotifierProvider);
     return Scaffold(
-      body: ListView.builder(
-        padding: const EdgeInsets.only(
-          top: 10,
-        ),
-        itemCount: itemList.length,
-        itemBuilder: (context, index) => ItemSellingCard(
-          itemDetail: itemList[index],
+      body: state.when(
+        // TODO: YOU COULD ADD NOT FOUND STATE AND SHOW EMPTY SCREEN THAT WAY
+        loading: () => const Center(child: CircularProgressIndicator()),
+        loaded: (items) {
+          items = items.where((item) => item.userId == user.id).toList();
+          items = items.where((item) {
+            final endDate = DateTime.parse(item.endDate);
+            final now = DateTime.now();
+            if (item.userId == user.id && now.isBefore(endDate)) {
+              return true;
+            }
+            return false;
+          }).toList();
+          if (items.isEmpty) {
+            return const EmptyScreen(
+              message: 'Try selling some items!',
+              icon: Icons.foundation,
+            );
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.only(
+              top: 10,
+            ),
+            itemCount: items.length,
+            itemBuilder: (context, index) => ItemSellingCard(
+              item: items[index],
+            ),
+          );
+        },
+        failed: (alert) => ErrorScreen(
+          onPressed: () => ref.read(itemsStateNotifierProvider.notifier).getItems(),
         ),
       ),
     );
